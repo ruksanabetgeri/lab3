f20da8fc121a379b35727677ce6e028e5b39a01e
ruksanabetgeri <ruksanabetageri300@gmai.com>
2025-05-12 21:15:04 +0530

    save

---
+++ Person.java
@@ -0,0 +1,20 @@
+package lab3;
+    import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    String name, dob;
+
+    public Person(String name, String dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public void displayPersonDetails() {
+        int age = Period.between(LocalDate.parse(dob), LocalDate.now()).getYears();
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+
+
